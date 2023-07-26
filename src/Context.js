de5f952c62601ae3a18a883f9c50a8b64e993ac4
timeisnotgood <bhoopathicl@gmail.com>
2023-07-26 21:41:58 +0530

    practice

--- src/Context.js
+++ src/Context.js
@@ -11,22 +11,28 @@ const Context = () => {
 
     const subhandler =(e)=>{
         e.preventDefault()
-        setdata(...data, detail)
-        setdetail({username : "", email:"", password:""})
-        localStorage.setItem("Reg detail", data)
+        setdata([detail, ...data])
+        setdetail({username : " ", email:" ", password:" "})
+
+        localStorage.setItem("Reg detail", JSON.stringify(data))
+        console.log(data);
+
     }
 
-    console.log(data);
+
+    const statehandler = (e) =>{
+        setdetail({...detail, [e.target.name]:e.target.value})
+    }
 
   return (
     <div>
         <form onSubmit={subhandler} >
             <lable>Username</lable>
-            <input type='text' value={detail.username} name='username' onChange={(e)=>setdetail({...detail, [e.target.name] : e.target.value})}   />
+            <input type='text' value={detail.username} name='username' onChange={statehandler}   />
             <lable>email</lable>
-            <input type='text' value={detail.email} name='email' onChange={(e)=>setdetail({...detail, [e.target.name] : e.target.value})}   />
-            <lable>Username</lable>
-            <input type='text' value={detail.password} name='password' onChange={(e)=>setdetail({...detail, [e.target.name] : e.target.value})}   />
+            <input type='text' value={detail.email} name='email' onChange={statehandler}   />
+            <lable>password</lable>
+            <input type='text' value={detail.password} name='password' onChange={statehandler}   />
             <button>Submit</button>
         </form>
         {JSON.stringify(data)}
