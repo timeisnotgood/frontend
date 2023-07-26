adcc09a4da90c610415caa55a7aa2e80f937badf
timeisnotgood <bhoopathicl@gmail.com>
2023-07-26 21:20:48 +0530

    practice

--- src/Context.js
+++ src/Context.js
@@ -2,37 +2,34 @@ import React, { createContext, useContext, useState } from 'react'
 
 const Context = () => {
 
-    const Checkinterface = createContext(null)
-    const Checkbox =({children}) =>{
-        const [state, setstate] = useState(false)
-        return(
-            <Checkinterface.Provider value={{state, setstate}} >{children}</Checkinterface.Provider>
-        )
+    const [detail, setdetail] = useState({
+        username : "",
+        email : "",
+        password : ""
+    })
+    const [data, setdata] = useState([])
+
+    const subhandler =(e)=>{
+        e.preventDefault()
+        setdata(...data, detail)
+        setdetail({username : "", email:"", password:""})
+        localStorage.setItem("Reg detail", data)
     }
 
-    const Input =()=>{
+    console.log(data);
 
-        const Context = useContext(Checkinterface)
-
-        const {state, setstate} = Context
-
-        return <input type={'checkbox'} onChange={(e)=>{
-            setstate(e.target.value)
-        }} checked={state} />
-    }
-    const Lable =({children})=>{
-
-        const Context = useContext(Checkinterface)
-
-        const {state, setstate} = Context
-        return <lable onClick={()=> setstate( (state)=> !state )} >{children}</lable>
-    }
   return (
     <div>
-        <Checkbox>
-            <Input/>
-            <Lable>Click here</Lable>
-        </Checkbox>
+        <form onSubmit={subhandler} >
+            <lable>Username</lable>
+            <input type='text' value={detail.username} name='username' onChange={(e)=>setdetail({...detail, [e.target.name] : e.target.value})}   />
+            <lable>email</lable>
+            <input type='text' value={detail.email} name='email' onChange={(e)=>setdetail({...detail, [e.target.name] : e.target.value})}   />
+            <lable>Username</lable>
+            <input type='text' value={detail.password} name='password' onChange={(e)=>setdetail({...detail, [e.target.name] : e.target.value})}   />
+            <button>Submit</button>
+        </form>
+        {JSON.stringify(data)}
     </div>
   )
 }
