a4f6190113e667fa01bc39a690ade37671a25a1a
timeisnotgood <bhoopathicl@gmail.com>
2023-08-03 13:23:22 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import Context from './Context'
-
+import { useState } from "react"
+import "./app.css"
 export default function App() {
+
+  const [data, setdata] = useState({
+    name : "",
+    email : "",
+    password : ""
+  })
+  const [info, setinfo] = useState([])
+
+  const datahandler = (e) =>{
+    setdata({...data, [e.target.name]:e.target.value})
+  }
+
+  const subhandler = async (e) =>{
+    e.preventDefault()
+    setinfo([...info, data])
+    setdata({name : "", email:"", password:""})
+    const response = await fetch('http://localhost:5000/first', {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(info)
+    })
+  }
+
 
   return (
     <div>
-    <Context/>
+      <h4>Register Here</h4>
+      <form className="form" onSubmit={subhandler} >
+        <input type="text" value={data.name} name="name" placeholder="Username" onChange={datahandler} />
+        <input type="text" value={data.email} name="email" placeholder="Email" onChange={datahandler} />
+        <input type="text" value={data.password} name="password" placeholder="Password" onChange={datahandler} />
+        <button>Submit</button>
+      </form>
     </div>
   )
   
