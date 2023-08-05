646a185523a41d47aa9254fc0b77b95c88a53c5f
timeisnotgood <bhoopathicl@gmail.com>
2023-08-05 15:47:35 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -14,9 +14,12 @@ export default function App() {
   }
 
   const subhandler = async (e) =>{
+
     e.preventDefault()
     setinfo([...info, data])
     setdata({name : "", email:"", password:""})
+
+
     const response = await fetch('http://localhost:5000/first', {
       method: 'POST',
       headers: {
@@ -24,11 +27,18 @@ export default function App() {
       },
       body: JSON.stringify(info)
     })
+
+
+    const da = response.json()
+    da.then( data =>{
+      console.log(data);
+    })
+
   }
 
 
   return (
-    <div>
+    <div className="main" >
       <h4>Register Here</h4>
       <form className="form" onSubmit={subhandler} >
         <input type="text" value={data.name} name="name" placeholder="Username" onChange={datahandler} />
