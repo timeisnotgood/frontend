cab284ba9e1683eb8a73566e64820dd2288c0d44
timeisnotgood <bhoopathicl@gmail.com>
2023-08-26 20:55:17 +0530

    cors

--- src/page/Contatclist.js
+++ src/page/Contatclist.js
@@ -1,17 +1,30 @@
 import React from 'react'
 
-const Contatclist = (props) => {
-  console.log(props);
-  // const names = list.map( name =>{
-  //   return(
-  //     <div key={name._id} >
-  //       <p>{name.name}</p>
-  //     </div>
-  //   )
-  // } )
+const Contatclist = ({list}) => {
+  
+  const deletehandler =async (id)=>{
+    console.log(id);
+    const data = await fetch('http://localhost:5000/contact', {
+      method : "DELETE",
+      headers:{
+        'Content-type' : 'application/json'
+      },
+      body:JSON.stringify(id)
+    })
+  }
+
   return (
     <div>
-      hai
+      {list.map( name =>{
+    return(
+      <div key={name._id} >
+        <p>
+          {name.name}
+          <button onClick={()=>deletehandler(name._id)} >Delete</button>
+        </p>
+      </div>
+    )
+  } )}
     </div>
   )
 }
