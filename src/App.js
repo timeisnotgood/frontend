66d34ac8bc7b0c17cd3a074776ec944051b6c9fe
timeisnotgood <bhoopathicl@gmail.com>
2023-08-26 20:07:48 +0530

    prac

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Contact from "./page/Contact"
-import Contatclist from "./page/Contatclist"
-import Contactdetail from "./page/Contactdetail"
-import { useContext, useState } from "react"
-
-
 
 export default function App() {
   return(
@@ -26,21 +20,3 @@ export default function App() {
 
 
 
-
-
-
-
-
-
-
-
-
-// <div>
-//       <BrowserRouter>
-//       <Header/>
-//         <Routes>
-//           <Route path='/product/:productId' exact element={<Productdetail/>} />
-//           <Route path='/list' exact element={<ProductList/>} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
