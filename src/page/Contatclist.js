7978cee6c0da6eb6d0c6709667cb4db1903db820
timeisnotgood <bhoopathicl@gmail.com>
2023-08-27 13:33:48 +0530

    prac

--- src/page/Contatclist.js
+++ src/page/Contatclist.js
@@ -2,23 +2,37 @@ import React, { useEffect, useState } from 'react'
 
 const Contatclist = () => {
   
-  const [list, setlist] = useState([])
+  const [list, setlist] = useState()
 
- useEffect(async()=>{
-  const data = await fetch('http://127.0.0.1:5000/findcontact', {
+ useEffect(()=>{
+  const datas =async ()=>{
+    const data = await fetch('http://127.0.0.1:5000/findcontact', {
       method : "GET",
       headers:{
         'Content-Type': 'application/json', // Specify content type
     },
     })
     const res = await data.json()
-    setlist([...list, res])
+    setlist(res.data)
+  }
+  datas();
  },[])
- 
- console.log(list);
+
+ const data = list.map( data =>{
+  return(
+    <div key={data._id} >
+      <p>{data.name}</p>
+      <button>Edit</button>
+      <button onClick={async()=>{
+        const data = await fetch('')
+      }} >Delete</button>
+    </div>
+  )
+ } )
+
   return (
     <div>
-    hai
+      {data}
     </div>
   )
 }
