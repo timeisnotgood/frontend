f2903aed202b55023d0d5227dfb3c98706b729ab
timeisnotgood <bhoopathicl@gmail.com>
2023-08-31 07:51:03 +0530

    CRUD COMPLETE

--- src/page/Editcontact.js
+++ src/page/Editcontact.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
+
 
 const Editcontact = () => {
+
+  const { id } = useParams()
+  
+  const [edit, setedit] = useState({
+    name : "",
+    email : "",
+    number : ""
+  }) 
+
+  // Change Handler
+  const handler = (e) =>{
+    setedit({...edit, [e.target.name] : e.target.value})
+  }
+
+  //Submit Handler
+  const subhandler = async (e) =>{
+    e.preventDefault();
+    const data = await fetch(`http://127.0.0.1:5000/updatecontact/${id}`,{
+      method:"PUT",
+      headers:{
+        'Content-Type' : "application/json"
+      },
+      body:JSON.stringify(edit)
+    })
+    const res = await data.json()
+    console.log(res);
+  }
+  
   return (
-    <div>Editcontact</div>
+    <div>
+      <form onSubmit={subhandler} >
+        <input type='text' placeholder='name' name='name' onChange={handler} />
+        <input type='text' placeholder='email' name='email' onChange={handler} />
+        <input type='text' placeholder='number' name='number' onChange={handler} />
+        <button>Submit Changes</button>
+      </form>
+    </div>
   )
 }
 
-export default Editcontact
+export default Editcontact 
