ca28cea48d7960fa01c75f1d795ce56d942cbced
timeisnotgood <bhoopathicl@gmail.com>
2023-11-08 18:33:13 +0530

    async work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
 import Rout from "./login/Route"
 import Contact from "./page/Contact"
 
 export default function App() {
+
+
+  function add(a, b){
+    console.log(a+b);
+  }
+
+
+  setInterval(() => {
+    console.log("useEffect:",5*5);
+    
+  }, 2000);
+
+  const sub = (a,b) =>{
+    console.log(a-b);
+  }
+
+  add(10,25)
+  sub(11,45)
+
+  
+
   return(
-    <Rout/>
+    // <Rout/>
+    <div>
+      synchronus vs Asynchronus
+    </div>
   )
 }
 
