8e1895c97fcea8b2e8ae304bae6af5a3aca4256a
timeisnotgood <bhoopathicl@gmail.com>
2023-11-09 20:42:55 +0530

    updated login

--- src/App.js
+++ src/App.js
@@ -4,31 +4,8 @@ import Contact from "./page/Contact"
 
 export default function App() {
 
-
-  function add(a, b){
-    console.log(a+b);
-  }
-
-
-  setInterval(() => {
-    console.log("useEffect:",5*5);
-    
-  }, 2000);
-
-  const sub = (a,b) =>{
-    console.log(a-b);
-  }
-
-  add(10,25)
-  sub(11,45)
-
-  
-
   return(
-    // <Rout/>
-    <div>
-      synchronus vs Asynchronus
-    </div>
+    <Rout/>
   )
 }
 
--- src/login/Home.js
+++ src/login/Home.js
@@ -1,43 +1,50 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
 
+  const navigate = useNavigate()
   const [user, setuser] = useState()
   const [loggedin, setloggedin] = useState(false)
 
   useEffect(()=>{
-        function setter(){
-        const token = localStorage.getItem("token")
+        async function setter(){
+        const token = await localStorage.getItem("token")
+        console.log(token);
         if(token == null){
           setloggedin(false)
-        }else{
+        }
           const base64Url = token.split('.')[1];
           const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
           const payload = JSON.parse(atob(base64));
           const currentuser = payload.user
+          console.log(currentuser);
           setuser(currentuser)
-          console.log(user);
           setloggedin(true)
         }
-        }
-        setter()
-  },[])
+        setter();
+  },[]);
 
   const logouthandler = () =>{
     localStorage.removeItem('token')
     setloggedin(false)
+    navigate('/')
   }
   
+  console.log(user);
+
 
   return (
     <div>
-    hello 
+    hello {user ? user.name : null}
     <div>
       {loggedin ? 
       <button onClick={logouthandler}>logout</button> 
       :  
-      <Link to={"/"} >login</Link>}
+      <Link to={"/"} >
+        <button>login</button>
+      </Link>}
     </div>
     </div>
   )
